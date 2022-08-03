941426cf09b5a53033a0a8ab7067b15de3e9b1df
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2022-08-03 15:05:33 +0300

    Update firebase_test_labTests.m

--- ios/firebase_test_labTests/firebase_test_labTests.m
+++ ios/firebase_test_labTests/firebase_test_labTests.m
@@ -5,32 +5,38 @@
 //  Created by Niall on 02.08.2022.
 //
 
-#import <XCTest/XCTest.h>
+// #import <XCTest/XCTest.h>
 
-@interface firebase_test_labTests : XCTestCase
+// @interface firebase_test_labTests : XCTestCase
 
-@end
+// @end
 
-@implementation firebase_test_labTests
+// @implementation firebase_test_labTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+// - (void)setUp {
+//     // Put setup code here. This method is called before the invocation of each test method in the class.
+// }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
+// - (void)tearDown {
+//     // Put teardown code here. This method is called after the invocation of each test method in the class.
+// }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
+// - (void)testExample {
+//     // This is an example of a functional test case.
+//     // Use XCTAssert and related functions to verify your tests produce the correct results.
+// }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+// - (void)testPerformanceExample {
+//     // This is an example of a performance test case.
+//     [self measureBlock:^{
+//         // Put the code you want to measure the time of here.
+//     }];
+// }
 
-@end
+// @end
+
+
+@import XCTest;
+@import integration_test;
+
+INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
