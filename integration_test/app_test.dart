744bb0f95c49c30e0b218bb55dbef7401aff67d3
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2022-08-03 13:01:17 +0300

    Update app_test.dart

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:inegrate/main.dart';
+import 'package:firebase_test_lab/main.dart';
 // import 'dart:io';
 
 void main() {
